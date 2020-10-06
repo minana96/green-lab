415c6c26f33143a887b3f32b6bf1f7217b6c1f96
Milica <60661838+minana96@users.noreply.github.com>
2020-10-06 23:29:14 +0200

    added after run script

--- android_runner/CriticalCSSExperiment/Scripts/after_run.py
+++ android_runner/CriticalCSSExperiment/Scripts/after_run.py
@@ -1,6 +1,8 @@
 from AndroidRunner.Device import Device
+from CriticalCSSExperiment.Scripts.Server import stop
 
 
 # noinspection PyUnusedLocal
 def main(device: Device, *args: tuple, **kwargs: dict):
-    pass
+    stop()
+
